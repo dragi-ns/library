02bfe68452d4291033aaf175b3eed7f8bf58c795
dragi-ns <54716240+dragi-ns@users.noreply.github.com>
2021-11-14 13:51:44 +0100

    Add library UI

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 (function IIFE() {
+    const root = document.documentElement;
+    if (localStorage.getItem('mode')) {
+        root.dataset.mode = localStorage.getItem('mode');
+    }
 
+    const toggleMode = document.querySelector('#toggle-mode');
+    toggleMode.addEventListener('click', () => {
+        let mode = null;
+        if (!root.dataset.mode || root.dataset.mode === 'light') {
+            mode = 'dark';
+        } else {
+            mode = 'light';
+        }
+        root.dataset.mode = mode;
+        localStorage.setItem('mode', mode);
+
+    });
 }());
